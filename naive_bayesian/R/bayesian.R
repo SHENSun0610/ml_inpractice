c401a9a3d4f197255fe2491d722ae3aae28a0de7
pingao777 <pingao777@gmail.com>
2015-10-11 13:43:17 +0800

    贝叶斯算法markdown完成，为贝叶斯R文件添加注释

--- naive_bayesian/R/bayesian.R
+++ naive_bayesian/R/bayesian.R
@@ -6,8 +6,7 @@ multicols <- function(df) {
   
   if (is.null(n)) {
     df
-  }
-  else if (n == 1) {
+  } else if (n == 1) {
     df[, 1]
   } else {
     df[, 1] * multicols(df[, 2:n])
@@ -20,8 +19,7 @@ pluscols <- function(df) {
   
   if (is.null(n)) {
     df
-  }
-  else if (n == 1) {
+  } else if (n == 1) {
     df[, 1]
   } else {
     df[, 1] + pluscols(df[, 2:n])
@@ -29,29 +27,54 @@ pluscols <- function(df) {
 }
 
 
-# 1.计算离散x的条件概率表
+# 计算xcol的条件概率，xcol为离散值
+#
+# Args:
+#   xcol: 特征列
+#   ycol: 类别列
+#   trainset: 训练集
+#
+# Returns:
+#   xcol的条件概率
 get.discrete.xtable <- function(xcol, ycol, trainset) {
   xcount <- ddply(trainset, c(ycol, xcol), each(count = nrow))
   xcount <- ddply(xcount, ycol, transform, ratio = count / sum(count))
   xtable <- xcount[["ratio"]]
-  # xratio<- as.vector(xratio)
   names(xtable) <- paste(xcount[[ycol]], xcount[[xcol]], sep = "")
   xtable
 }
 
+
+# 计算xcol的概率密度，xcol为连续值
+#
+# Args:
+#   xcol: 特征列
+#   ycol: 类别列
+#   trainset: 训练集
+#
+# Returns:
+#   xcol的高斯分布概率密度
 get.continout.xdensity <- function(xcol, ycol, trainset) {
   xmeansd <- ddply(trainset, ycol, function(x) c(mean = mean(x[[xcol]]), sd = sd(x[[xcol]])))
   mean <- xmeansd[["mean"]]
   names(mean) <- xmeansd[[ycol]]
   sd <- xmeansd[["sd"]]
-  sd[is.na(sd)] <- Inf
+  # 当某个分类下所有特征值相同时，付给sd一个极小值，因为高斯分布sd为0，密度值为无穷
+  sd[sd <= 0] <- 0.0001
   names(sd) <- xmeansd[[ycol]]
   xtable <- list(mean = mean, sd = sd)
-  
   xtable
 }
 
-# 2.计算各个y的概率表
+
+# 计算各个类别的概率
+#
+# Args:
+#   ycol: 类别列
+#   trainset: 训练集
+#
+# Returns:
+#   各个类别的概率
 get.ytable <- function(ycol, trainset) {
   ycount <- ddply(trainset, ycol, each(count = nrow))
   ycount$ratio <- ycount$count / nrow(trainset)
@@ -60,18 +83,34 @@ get.ytable <- function(ycol, trainset) {
   ytable
 }
 
-# 3.当某个分类中没有相应值时设置默认值
-get.defaultx <- function(ycol, trainset) {
+
+# Laplace校准，当某个类别下某个条件值概率为0时为其设置默认值
+#
+# Args:
+#   ycol: 类别列
+#   trainset: 训练集
+#
+# Returns:
+#   各个类别的Laplace校准概率值
+get.defaultx <- function(ycol, trainset, laplace) {
   ycount <- ddply(trainset, ycol, each(count = nrow))
-  defaultx <- 1 / ycount[["count"]]
+  defaultx <- laplace / ycount[["count"]]
   names(defaultx) <- ycount[[ycol]]
   defaultx
 }
 
 
-# 4.将测试集数据转换为相应的条件概率
-# 查找xcol列值分别为x和y的条件概率值
-testset2plist <- function(testset, discrete_xtable, continous.xdensity, defaultx) {
+# 将训练集的值转换为相应的条件概率
+#
+# Args:
+#   testset: 测试集
+#   discrete.xtable: 离散值得条件概率
+#   continous.xdensity: 连续值得高斯分布概率密度
+#   defaultx: Laplace校准概率
+#
+# Returns:
+#   长度等于类别数的列表，列表的每个元素都是一个条件概率组成的矩阵
+testset2plist <- function(testset, discrete.xtable, continous.xdensity, defaultx) {
   n <- ncol(testset)
   cnames <- names(testset)
   names(cnames) <- cnames
@@ -86,14 +125,15 @@ testset2plist <- function(testset, discrete_xtable, continous.xdensity, defaultx
         mean <- continous.xdensity[[xn]][["mean"]][[y]]
         sd <- continous.xdensity[[xn]][["sd"]][[y]]
         p <- dnorm(testset[[xn]], mean, sd)
+        p[is.na(p)] <- 0
         p
       } else {
         index <- paste(y, testset[[xn]], sep = "")
-        p <- discrete_xtable[[xn]][index]
+        p <- discrete.xtable[[xn]][index]
         p[is.na(names(p))] <- defaultx[[y]]
         p 
       }
-      
+
     }))
     unname(pm)
   })
@@ -102,7 +142,8 @@ testset2plist <- function(testset, discrete_xtable, continous.xdensity, defaultx
 }
 
 
-bayesian <- function(trainset) {
+# 对上面各个方法的封装
+bayesian <- function(trainset, laplace = 1) {
   colname <- names(trainset)
   n <- ncol(trainset)
   xcols <- colname[1:n-1]
@@ -116,21 +157,26 @@ bayesian <- function(trainset) {
   continous.xdensity <- lapply(continous.xcols, get.continout.xdensity, ycol, trainset)
   names(continous.xdensity) <- continous.xcols
  
-  
   ytable <- get.ytable(ycol, trainset)
-  defaultx <- get.defaultx(ycol, trainset)
+  defaultx <- get.defaultx(ycol, trainset, laplace)
   
   list(cxtable = continous.xdensity, dxtable = discrete.xtable, ytable = ytable, defaultx = defaultx)
 }
 
 
-prodict <- function(testset, bayesian) {
+# 根据测试集的已知条件预测类别值
+#
+# Args:
+#   testset: 测试集
+#   bayesian: bayesian函数返回的结果
+#
+# Returns:
+#   测试集的预测类别值
+forecast <- function(testset, bayesian) {
   plist <- testset2plist(testset, bayesian[["dxtable"]], 
                          bayesian[["cxtable"]], bayesian[["defaultx"]])
   
   pmatrix <- do.call("rbind", lapply(plist, multicols)) * bayesian[["ytable"]]
-  # pmatrix <- do.call("rbind", lapply(plist, function(x)pluscols(log10(x)))) +
-    # log10(bayesian[["ytable"]])
   pmatrix <- t(pmatrix)
   
   forecast <- colnames(pmatrix)[max.col(pmatrix)]
--- naive_bayesian/R/test.R
+++ naive_bayesian/R/test.R
@@ -1,12 +1,12 @@
 rm(list = ls())
 
+
 library(e1071)
 source('R/bayesian.R', encoding = "UTF-8")
 load("data/houses_30000.RData")
 
 
-shoot  <- function(trainsize, testsize, data, times = 5) {
-  # cols <- c(xcols, ycol)
+shoot <- function(trainsize, testsize, data, times = 5) {
   num.basket <- 1:nrow(data)
   ratio <- 0
   ratio <- rep(0, times)
@@ -20,13 +20,13 @@ shoot  <- function(trainsize, testsize, data, times = 5) {
     test.set <- data[sel2, ]
     
     b <- bayesian(training.set)
-    result <- prodict(test.set, b)
+    result <- forecast(test.set, b)
     b2 <- naiveBayes(training.set[, 1:ncol(training.set)-1], training.set[, ncol(training.set)])
     result2 <- predict(b2, test.set[, 1:ncol(test.set)-1])
     hit <- test.set[, ncol(test.set)] == result
-    ratio <- sum(hit) / testsize
+    ratio[i] <- sum(hit) / testsize
     hit2 <- test.set[, ncol(test.set)] == result2
-    ratio2 <- sum(hit2) / testsize
+    ratio2[i] <- sum(hit2) / testsize
   }
   c(mean(ratio), mean(ratio2))
 }
@@ -38,15 +38,14 @@ test <- function(minshots, maxshots, len, data) {
   ratio2 <- rep(0, len)
   for (i in seq_along(shots)) {
     cat("=")
-    r <- shoot(shots[i], 10, data, 1)
+    r <- shoot(shots[i], 30, data, 3)
     ratio[i] <- r[1]
     ratio2[i] <- r[2]
   }
-  plot(shots, ratio, type = "l", ylim = c(0, 1))
+  plot(shots, ratio, type = "l", ylim = c(0, 1), xlab = "训练集样本数", ylab = "正确率")
   lines(shots, ratio2, lty = 3, col = "red")
   legend("bottomright",legend = c("mine", "e1071"), 
          col = c("black", "red"), lty = c(1,3), 
          cex = 0.8)
-  # abline(h = mean(ratio), lty = 2, col ="red")
 }
 
